5509c3022f8b7bbd3c0432e68f1aa1d08bfd8af4
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-12-10 20:40:39 +0100

    fundamentos ios practica, empezada

---
+++ Fundamentos_iOS3/Controller/SplashViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Fundamentos_iOS3
+//
+//  Created by Ricardo Hernandez on 13/12/20.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    //private let SEGUE_TO_MAIN = "SEGUE_FROM_LAUNCH_TO_MAIN"
+    
+    // View Properties
+    @IBOutlet var activityIndicator: UIActivityIndicatorView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        // iniciamos la animación
+        activityIndicator?.startAnimating()
+        
+        //performSegue(withIdentifier: SEGUE_TO_MAIN, sender: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //ocultar navigation bar
+        navigationController?.isNavigationBarHidden = true
+    }
+
+
+}
+
